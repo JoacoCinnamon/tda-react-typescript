fc48beae844065031b06d537fd1b167910c6946f
JoacoCinnamon <joacopontello2002@gmail.com>
2023-04-11 04:53:55 -0300

    TDA Main Page (only append node feature)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,67 @@
 import { useState } from "react";
+import { LinkedList } from "./LinkedList";
+
 function App() {
-  const [count, setCount] = useState(0);
+  const Node = ({ value }: { value: string }) => {
+    return (
+      <div className="text-2xl rounded-2xl bg-blue-700  p-4 hover:animate-spin">
+        {value}
+      </div>
+    );
+  };
+  const Nodes = () => {
+    if (linkedList.hasHead()) {
+      const values = linkedList.getValues();
+      return (
+        <>
+          {values.map((value, idx) => (
+            <Node value={value.toString()} key={idx} />
+          ))}
+        </>
+      );
+    }
+    return <></>;
+  };
+
+  const [linkedList, setlinkedList] = useState<LinkedList<Number>>(
+    new LinkedList<number>()
+  );
+  const [nodeValue, setNodeValue] = useState("");
+  const nodeValueAsNumber = Number(nodeValue);
+
+  function handleAppendNode(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const newLinkedList = linkedList.append(nodeValueAsNumber);
+    setlinkedList(newLinkedList);
+    setNodeValue("");
+  }
 
   return (
     <div className="App">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="p-12 flex gap-6">
+        <Nodes />
       </div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div
+        className=" 
+        fixed
+        inset-x-0
+        bottom-0
+        p-4 flex flex-auto justify-around mb-4"
+      >
+        <form className="form flex flex-col gap-2" onSubmit={handleAppendNode}>
+          <input
+            type="number"
+            required
+            className="bg-slate-500 rounded-full p-1 w-14 text-center"
+            placeholder="Value"
+            value={nodeValue}
+            onChange={(e) => setNodeValue(e.target.value)}
+          />
+          <button className="p-3 rounded-full bg-zinc-700 hover:bg-zinc-600 ">
+            Add
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="flex justify-center p-4 dark:bg-zinc-700">
+      <h1 className="text-5xl">Data Structures</h1>
+    </header>
+  );
+}
+
+export default Header;
